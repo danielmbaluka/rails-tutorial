cec6e25b4bcbc53d54080ebb35554561eacb2387
danielmbaluka <danielmbaluka@gmail.com>
2019-03-16 18:48:43 +0300

    user sign up

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,4 +1,20 @@
 class AuthController < ApplicationController
   def signup
+  	@user = User.new
   end
+
+  def create
+  	@user = User.new(user_params)
+  	if @user.save
+  		flash[:success] = "Your account has been created successfully!"
+  	  	redirect_to root_url
+  	else
+  		render :signup
+  	end
+  end
+
+  private
+  	def user_params
+  		params.require(:user).permit(:email, :name, :password, :password_confirmation)
+  	end
 end
